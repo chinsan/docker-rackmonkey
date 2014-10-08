8f201c0fb1602694fb7e7a9a80865c435a365fea
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-08 10:21:41 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#this for reference only to create the backup scrips for each container ... the idea to use the same command for each container
+#each container will have their own custum backup scritp for it ... 
+
+#Backup important file ... of the configuration ...
+  cp  /etc/hosts  /var/backups/
